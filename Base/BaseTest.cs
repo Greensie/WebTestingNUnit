478e49aeed2b30b55f8f1c729b0c075c4963a531
Marcel <51127184+Greensie@users.noreply.github.com>
2025-06-03 21:54:31 +0200

    Dodaj pliki projektów.

---
+++ Base/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebTestingNUnit.Base
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Tests/TC_001.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebTestingNUnit.Base;
+
+namespace WebTestingNUnit.Tests
+{
+    public class TC_001 : BaseTest
+    {
+        [Test]
+        public void Should_Open_Page()
+        {
+            Assert.That(driver.Title, Does.Contain("Swag Labs"));
+        }
+    }
+}
