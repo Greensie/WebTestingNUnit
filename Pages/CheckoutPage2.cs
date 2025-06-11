6ac594a489dafe8ac7728f707efb81f5d420a827
Marcel <51127184+Greensie@users.noreply.github.com>
2025-06-11 18:53:46 +0200

    checkout page2, TC_005

---
+++ Pages/CheckoutPage2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace WebTestingNUnit.Pages
+{
+    public class CheckoutPage2
+    {
+        readonly string cancelSelector = "#cancel";
+        readonly string finishSelector = "#finish";
+        readonly string itemTotalSelector = "#checkout_summary_container > div > div.summary_info > div.summary_subtotal_label";
+        readonly string taxSelector = "#checkout_summary_container > div > div.summary_info > div.summary_tax_label";
+        readonly string totalSelector = "#checkout_summary_container > div > div.summary_info > div.summary_total_label";
+        readonly string shippingSelector = "#checkout_summary_container > div > div.summary_info > div:nth-child(4)";
+        readonly string paymentSelector = "#checkout_summary_container > div > div.summary_info > div:nth-child(2)";
+
+       /**************************************************************************************************************
+       * Method for proceeding to finish shopping.
+       **************************************************************************************************************/
+        public void FinishShopping(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector(finishSelector)).Click();
+        }
+
+       /**************************************************************************************************************
+       * Method for leaving recipt page.
+       **************************************************************************************************************/
+        public void CancelShopping(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector(cancelSelector)).Click();
+        }
+
+       /**************************************************************************************************************
+       * Method for checking item final prices. If option is invalid or not specified it returns prices with tax included.
+       * If option is specified it returns:
+       * 0 - item total without taxes
+       * 1 - tax value
+       * 2 - items with tax included
+       **************************************************************************************************************/
+        public string GetPrices(IWebDriver driver, int option)
+        {
+            string[] webdata = {driver.FindElement(By.CssSelector(itemTotalSelector)).Text,
+                driver.FindElement(By.CssSelector(taxSelector)).Text,
+                driver.FindElement(By.CssSelector(totalSelector)).Text };
+
+            if (option == 0)
+            {
+                return webdata[0];
+            }
+
+            if (option == 1)
+            {
+                return webdata[1];
+            }
+
+            return webdata[2];
+        }
+
+       /**************************************************************************************************************
+       * Method for getting a shipping information.
+       **************************************************************************************************************/
+        public string GetShippingInformation(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector(shippingSelector)).Text;
+        }
+
+       /**************************************************************************************************************
+       * Method for getting a payment information.
+       **************************************************************************************************************/
+        public string GetPaymentInformation(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector(paymentSelector)).Text;
+        }
+
+       /**************************************************************************************************************
+       * Method for checking if driver is on checkout page one.
+       **************************************************************************************************************/
+        public bool isAt(IWebDriver driver)
+        {
+            return driver.Url.Contains("checkout-step-two");
+        }
+    }
+}
---
+++ Tests/TC_005/TC_005_WholeBuyProcedure_Chrome_Headless.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTestingNUnit.Base;
+using WebTestingNUnit.Components;
+using WebTestingNUnit.Pages;
+
+namespace WebTestingNUnit.Tests.TC_005
+{
+    public class TC_005_WholeBuyProcedure_Chrome_Headless : BaseTestHeadless
+    {
+        [Test]
+        public void TC_005_WholeBuyProcedure()
+        {
+            var loginPage = new LoginPage();
+            var inventoryPage = new InventoryPage();
+            var cartPage = new CartPage();
+            var checkoutPage1 = new CheckoutPage1();
+            var checkoutPage2 = new CheckoutPage2();
+            var logoutComponent = new LogoutComponent();
+            string exp1 = "Item total: $55.97";
+            string exp2 = "Tax: $4.48";
+            string exp3 = "Total: $60.45";
+            string expShipping = "Free Pony Express Delivery!";
+            string expPayment = "SauceCard #31337";
+
+            string[] itemsShort = { "backpack", "bike light", "t-shirt" };
+
+            Assert.That(loginPage.isAt(driver), "Login page was not loaded correctly.");
+            loginPage.LoginAsStandardUser(driver);
+            Assert.That(inventoryPage.isAt(driver),"Entered inventory Page sucesfully");
+
+            foreach (var item in itemsShort)
+            {
+                inventoryPage.AddItemToCart(driver, item);
+                Thread.Sleep(50);
+            }
+            int itemsInCartAfterAdding = inventoryPage.GetCartItemCount(driver);
+            Assert.That(itemsInCartAfterAdding, Is.EqualTo(3), "All items added to the cart!");
+
+            Thread.Sleep(150);
+
+            inventoryPage.EnterCart(driver);
+            Thread.Sleep(150);
+            Assert.That(cartPage.isAt(driver),"Enteret cart Page sucesfully");
+
+            cartPage.proceedToPayment(driver);
+            Thread.Sleep(150);
+
+            Assert.That(checkoutPage1.isAt(driver), "Entered checkout proceedure");
+            checkoutPage1.enterAllData(driver, "John", "Snow", "99-999");
+            checkoutPage1.proceedFurther(driver);
+            Thread.Sleep(150);
+
+            Assert.That(checkoutPage2.isAt(driver), "Entered final checkout page");
+            string act1 = checkoutPage2.GetPrices(driver, 0);
+            string act2 = checkoutPage2.GetPrices(driver, 1);
+            string act3 = checkoutPage2.GetPrices(driver, 2);
+            Assert.That(act1 == exp1, "Item total prices match!");
+            Assert.That(act2 == exp2, "Tax match!");
+            Assert.That(act3 == exp3, "Total prices match!");
+
+            string actShipping = checkoutPage2.GetShippingInformation(driver);
+            string actPayment = checkoutPage2.GetPaymentInformation(driver);
+            Assert.That(actShipping == expShipping, "Shipping info match!");
+            Assert.That(actPayment == expPayment, "Payment info match!");
+
+            checkoutPage2.FinishShopping(driver);
+            Thread.Sleep(150);
+            Assert.That(driver.Url.Contains("checkout-complete"), "User finished shopping!");
+
+            logoutComponent.Logout(driver);
+            Thread.Sleep(50);
+            Assert.That(logoutComponent.IsLoggedOut(driver), "User logged out!");
+        }
+    }
+}
